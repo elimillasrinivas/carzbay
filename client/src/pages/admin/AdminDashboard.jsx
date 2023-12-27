a58c60ceaa7569262c2d0a9849c73ae7709a9673
Srinivas Elimilla <114722049+elimillasrinivas@users.noreply.github.com>
2023-12-28 00:17:01 +0530

    Update AdminDashboard.jsx

--- client/src/pages/admin/AdminDashboard.jsx
+++ client/src/pages/admin/AdminDashboard.jsx
@@ -4,9 +4,31 @@ import { useNavigate } from 'react-router-dom'
 const AdminDashboard = () => {
   const navigate = useNavigate()
   return (
-    <div>
-      <button onClick={()=>{navigate("/admin/data-entry")}}>Enter Data</button>
-    </div>
+   <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+  <button
+    style={{
+      padding: '10px',
+      backgroundColor: '#3498db', // Background color
+      color: '#ffffff', // Text color
+      border: 'none',
+      borderRadius: '5px',
+      cursor: 'pointer',
+      transition: 'background-color 0.3s',
+    }}
+    onClick={() => {
+      navigate("/admin/data-entry");
+    }}
+    onMouseOver={(e) => {
+      e.target.style.backgroundColor = '#2980b9'; // Hover background color
+    }}
+    onMouseOut={(e) => {
+      e.target.style.backgroundColor = '#3498db'; // Restore background color on hover out
+    }}
+  >
+    Enter Data
+  </button>
+</div>
+
   )
 }
 
